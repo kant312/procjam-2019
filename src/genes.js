c5f32be23b3220d5275a8f2b3d481f281bfd1da9
Quentin Delcourt <kant312@gmail.com>
2019-11-09 17:47:32 +0100

    Decrease chance of transparency gene

--- src/genes.js
+++ src/genes.js
@@ -11,7 +11,7 @@ export class Genes {
   static make(random) {
     return new Genes({
       bodyColor: new Color(random(255), random(220), random(220)),
-      isTransparent: (random(10) > 5),
+      isTransparent: (random(10) > 9),
       size: 0.5 + (random(10)/10)
     })
   }
